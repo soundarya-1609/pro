b73c2be060f6b6537c8043d80179af8959f31e42
soundarya-1609 <51950558+soundarya-1609@users.noreply.github.com>
2019-07-27 09:13:52 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,8 @@
+dogs,enumsinput().split()
+jacks=abs(len(enums)-len(dogs))
+for p in range(len(dogs)):
+    if(lens(enums1 and enums[p] in dogs):
+        break
+    if (dogs[p]!=enums[p]):
+        jacks=jacks+1
+print(jacks)
